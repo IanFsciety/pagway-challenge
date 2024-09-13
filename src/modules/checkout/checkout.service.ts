37ad1ec29f84bfc65ee5ffd80887a8a4ee701761
IanFsciety <iandelas123@outlook.com>
2024-09-12 23:04:36 -0400

    create checkout service

---
+++ src/modules/checkout/checkout.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateCheckoutDto } from './dto/create-checkout.dto';
+
+@Injectable()
+export class CheckoutService {
+  constructor(private prisma: PrismaService) {}
+
+  createCheckout(createCheckoutDto: CreateCheckoutDto) {
+    const {
+      amount,
+      description,
+      cardNumber,
+      cardHolderName,
+      expirationDate,
+      cvv,
+      userId,
+    } = createCheckoutDto;
+
+    const lastFourDigits = `xxxxxxxxxxxx${cardNumber.slice(-4)}`;
+
+    return this.prisma.checkout.create({
+      data: {
+        amount,
+        description,
+        cardNumber: lastFourDigits,
+        cardHolderName,
+        expirationDate,
+        cvv,
+        userId,
+      },
+    });
+  }
+
+  findAllByUserId(userId) {
+    return this.prisma.checkout.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        payables: true,
+      },
+    });
+  }
+
+  findOne(id: string) {
+    return this.prisma.checkout.findUnique({
+      where: { id },
+      include: {
+        payables: true,
+      },
+    });
+  }
+}
