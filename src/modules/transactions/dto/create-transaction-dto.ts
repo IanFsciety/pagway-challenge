724860118ef2d5b7c1326a2105868c2973140495
IanFsciety <iandelas123@outlook.com>
2024-09-14 21:27:22 -0400

    use dinamics values

--- src/modules/transactions/dto/create-transaction-dto.ts
+++ src/modules/transactions/dto/create-transaction-dto.ts
@@ -1,26 +1,6 @@
-import { TransactionStatus } from '@prisma/client';
-import {
-  IsDateString,
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-  IsUUID,
-} from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateTransactionDto {
-  @IsNotEmpty()
-  @IsNumber()
-  amount: number;
-
-  @IsNotEmpty()
-  @IsEnum(TransactionStatus, { message: 'Status must be PENDING or PAID' })
-  status: TransactionStatus;
-
-  @IsNotEmpty()
-  @IsDateString()
-  paymentDate: string;
-
   @IsNotEmpty()
   @IsString()
   @IsUUID()
--- src/modules/transactions/dto/update-transaction-dto.ts
+++
@@ -1,8 +0,0 @@
-import { TransactionStatus } from '@prisma/client';
-import { IsEnum, IsNotEmpty } from 'class-validator';
-
-export class UpdateTransactionDto {
-  @IsNotEmpty()
-  @IsEnum(TransactionStatus, { message: 'Status must be PENDING or PAID' })
-  status: TransactionStatus;
-}
